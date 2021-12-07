18f6f79d3e93d62035b17c6e74cac0331e2b10b8
Syed Shahid <syedshahid1215@gmail.com>
2021-12-07 19:14:25 +0500

    passing data b/w screens using react-navigation-stack

--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -1,17 +1,13 @@
 import React from "react";
-import {View, Text, Button} from "react-native";
+import {View, Text} from "react-native";
 import { globalStyles } from "../styles/globalStyles";
 
-export default function ReviewDetails({navigation}) {
-
-    const pressHandler = () => {
-        navigation.goBack();
-    };
-
+export default function ReviewDetails({route,navigation}) {
     return(
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>Review Details Screen</Text>
-            <Button title="back to Home screen" onPress={pressHandler} />
+            <Text style={globalStyles.titleText}>title: { route.params.title }</Text>
+            <Text style={globalStyles.titleText}>body: { route.params.body }</Text>
+            <Text style={globalStyles.titleText}>rating: { route.params.rating }</Text>
         </View>
     )
 }
